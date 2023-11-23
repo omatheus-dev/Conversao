5de4de9f624b6071d8d2c8df72c914ff250c2ec5
Matheus Ferreira <matheusfer.dev@gmail.com>
2023-11-23 18:03:07 -0300

    Initial commit

---
+++ src/Wrapper.java
@@ -0,0 +1,13 @@
+public class Wrapper {
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+        System.out.println(numero);
+
+        Integer numero2 = numero;
+        System.out.println(numero2);
+        
+    }
+
+}
